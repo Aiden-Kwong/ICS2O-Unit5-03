bd167e39efdf371a494591cbc754a656794dc98a
AidenKwong <>
2021-06-02 13:09:44 +0000

    comment touch up

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // This event listener detects when the button is clicked.
 document.getElementById('button').addEventListener('click', message)
 
-// The function 'message' displays the value of the id 'input', which is the text field.
+// The function 'message' displays the value of the id 'input', which is the text field, as an alert.
 function message () {
   alert(document.getElementById('input').value)
 }
